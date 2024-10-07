e8fe4bf71739d7c5b66b8c2ae3ba0f258bfad24a
Mafe H <mafehernandezo0312@gmail.com>
2024-10-07 01:35:03 -0500

    header responsive

--- src/components/shared/DarkMode/DarkMode.tsx
+++ src/components/shared/DarkMode/DarkMode.tsx
@@ -1,37 +1,41 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const DarkMode = () => {
-    // Función que alterna el modo oscuro
-    const toggleDarkMode = () => {
-        const htmlElement = document.querySelector("html");
-        if (htmlElement) {
-            htmlElement.classList.toggle("dark");
-        }
-    };
+  // estado para controlar si el modo oscuro está activado o no
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
-    // Efecto para manejar el evento del botón
-    useEffect(() => {
-        const toggleButton = document.querySelector("#toggle");
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
 
-        // Asegúrate de que el botón existe antes de agregar el evento
-        if (toggleButton) {
-            toggleButton.addEventListener("click", toggleDarkMode);
-        }
+  // efecto para aplicar la clase "dark" 
+  useEffect(() => {
+    const htmlElement = document.querySelector("html");
 
-        // Limpiar el efecto eliminando el event listener
-        return () => {
-            if (toggleButton) {
-                toggleButton.removeEventListener("click", toggleDarkMode);
-            }
-        };
-    }, []); // El arreglo vacío significa que se ejecutará una vez al montar
+    if (isDarkMode) {
+      htmlElement?.classList.add("dark");
+    } else {
+      htmlElement?.classList.remove("dark");
+    }
+  }, [isDarkMode]); // se ejecuta cada vez que isDarkMode cambia
 
-    return (
-        <button id="toggle">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-moon" viewBox="0 0 16 16">
-                <path className="fill-current text-primary dark:text-white" fillRule="evenodd" d="M14.53 10.53a7 7 0 0 1-9.058-9.058A7.003 7.003 0 0 0 8 15a7.002 7.002 0 0 0 6.53-4.47z"/>
-            </svg>
-        </button>
-    );
-}
+  return (
+    <button id="toggle" onClick={toggleDarkMode}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="30"
+        height="30"
+        fill="currentColor"
+        className="bi bi-moon"
+        viewBox="0 0 16 16"
+      >
+        <path
+          className="fill-current text-white dark:text-white"
+          fillRule="evenodd"
+          d="M14.53 10.53a7 7 0 0 1-9.058-9.058A7.003 7.003 0 0 0 8 15a7.002 7.002 0 0 0 6.53-4.47z"
+        />
+      </svg>
+    </button>
+  );
+};
--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -1,24 +1,104 @@
-import Link from 'next/link';
-import { DarkMode } from '../DarkMode';
+import Link from "next/link";
+import { DarkMode } from "../DarkMode";
 
 export const Header = async () => {
-    return (
-        <header className="flex items-center justify-between w-full h-16 bg-blue-600 pr-8 dark:bg-blue-950">
-          <div className="flex items-center text-white">
-            <div className="text-xl font-bold bg-black h-16 px-8 pt-4">UNF</div>
-            <nav className="hidden md:flex space-x-12 pl-12 ">
-              <Link href="/" className="text-white hover:text-blue-500">Home</Link>
-              <Link href="/evets" className="text-white hover:text-blue-500">Events</Link>
-              <Link href="/contact" className="text-white hover:text-blue-500">Contact</Link>
-              <Link href="/about" className="text-white hover:text-blue-500">About</Link>
+  return (
+    <>
+      {/* Header for larger screens */}
+      <header className="hidden md:flex items-center justify-between w-full h-16 bg-blue-600 pr-8 dark:bg-blue-950 fixed z-10 top-0">
+        <div className="flex items-center text-white">
+          <div className="text-xl font-bold bg-black h-16 px-8 pt-4">UNF</div>
+          <nav className="hidden md:flex space-x-12 pl-12">
+            <Link href="/" className="text-white hover:text-blue-500">
+              Home
+            </Link>
+            <Link href="/events" className="text-white hover:text-blue-500">
+              Events
+            </Link>
+            <Link href="/contact" className="text-white hover:text-blue-500">
+              Contact
+            </Link>
+            <Link href="/about" className="text-white hover:text-blue-500">
+              About
+            </Link>
+          </nav>
+        </div>
 
-            </nav>
-          </div>
-    
-          {/* Botón Iniciar Sesión */}
-          <div>
-            <DarkMode />
-          </div>
-        </header>
-      );
-}
+        <div className="flex items-center space-x-6">
+          <DarkMode />
+          <Link href="/login">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              fill="currentColor"
+              className="bi bi-person-fill"
+              viewBox="0 0 16 16"
+            >
+              <path
+                className="fill-current text-white dark:text-white"
+                d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"
+              />
+            </svg>
+          </Link>
+          <Link href="/">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              fill="currentColor"
+              className="bi bi-search"
+              viewBox="0 0 16 16"
+            >
+              <path
+                className="fill-current text-white dark:text-white"
+                d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"
+              />
+            </svg>
+          </Link>
+        </div>
+      </header>
+
+      {/* Header for small screens */}
+      <header className="md:hidden flex items-center align-middle w-full h-16 bg-blue-600 pr-8 dark:bg-blue-950 fixed z-10 bottom-0">
+        <div className="flex items-center text-white justify-start">
+          <div className="text-xl font-bold bg-black h-16 px-8 pt-4">UNF</div>
+        </div>
+
+        <div className="flex items-center space-x-9 p-12 justify-center" >
+          <DarkMode />
+          <Link href="/login">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              fill="currentColor"
+              className="bi bi-person-fill"
+              viewBox="0 0 16 16"
+            >
+              <path
+                className="fill-current text-white dark:text-white"
+                d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"
+              />
+            </svg>
+          </Link>
+          <Link href="/">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              fill="currentColor"
+              className="bi bi-search"
+              viewBox="0 0 16 16"
+            >
+              <path
+                className="fill-current text-white dark:text-white"
+                d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"
+              />
+            </svg>
+          </Link>
+        </div>
+      </header>
+    </>
+  );
+};
