11f86152fcd98700fb1bbf75104e47680efba7a0
Mafe H <mafehernandezo0312@gmail.com>
2024-10-13 23:56:32 -0500

    ajuste de pantalla

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center md:min-h-screen">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center lg:min-h-screen">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="w-full h-auto object-cover"
